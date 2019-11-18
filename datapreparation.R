8fd62c526f7bb34ebfd8f940e37fdd67e87de6c6
Schattman <12001003308380@FEDIDCARD.GOV>
2019-11-18 15:56:43 -0500

    Initailize workspace

---
+++ datapreparation.R
@@ -0,0 +1,6 @@
+##### Turn the Tap Data Prepatation file
+##### Created: November 19, 2019
+##### Last updated: November 19, 2019
+##### Author: Rachel Schattman
+
+##### Section 1: Import data sets
